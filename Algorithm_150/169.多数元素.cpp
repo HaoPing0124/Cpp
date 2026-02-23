40a903ae52ad204c4b5af68d3429c3dcbc8cb182
Hao_Ping <2429302207@qq.com>
2026-02-23 20:01:53 +0800

    169.多数元素

---
+++ Algorithm_150/169.多数元素.cpp
@@ -0,0 +1,14 @@
+﻿//169.多数元素
+//给定一个大小为 n 的数组 nums ，返回其中的多数元素。
+//多数元素是指在数组中出现次数 大于 ⌊ n / 2 ⌋ 的元素。
+//你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+#include"Algorithm_150.h"
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};
